327f73b0c09065d714a1882a67938628a7391300
Pushp Singh <pushp.singh@SW-LP02545.local>
2020-06-23 22:23:40 +0530

    dev done

--- src/scripts/excelGenerator.js
+++ src/scripts/excelGenerator.js
@@ -1,8 +1,34 @@
+/**
+ * Showing/hiding clear button
+ */
+if (localStorage.getItem('rowCount') !== null) {
+    document.getElementById('clear-btn').style.visibility = "visible";
+} else {
+    document.getElementById('clear-btn').style.visibility = "hidden";
+}
+
+/**
+ * Clear excel sheet
+ */
+const clearExcelFile = function (event) {
+    event.preventDefault();
+    localStorage.clear()
+    location.reload();
+}
+
 const generateExcel = function (event) {
     event.preventDefault();
 
-    let rowCount = document.getElementById("rowCount").value;
-    let colCount = document.getElementById("colCount").value;
+    let existingRowCount = localStorage.getItem('rowCount');
+    let existingColCount = localStorage.getItem('colCount');
+
+    console.log(existingRowCount, existingColCount)
+
+    let rowCountFromUser = document.getElementById("rowCount").value;
+    let columnCountFromUser = document.getElementById("colCount").value;
+
+    let rowCount = rowCountFromUser === "" ? (existingRowCount !== null ? existingRowCount : "") : rowCountFromUser;
+    let colCount = columnCountFromUser === "" ? (existingColCount !== null ? existingColCount : "") : columnCountFromUser;
 
     if (rowCount === "" || rowCount <= 0) {
         window.alert("Row count cannnot be empty or less than 0");
@@ -25,16 +51,16 @@ const generateExcel = function (event) {
 let rCount = localStorage.getItem('rowCount');
 let cCount = localStorage.getItem('colCount');
 
-// const button = document.createElement('input');
-// button.setAttribute("type", 'button');
-// cardNumber.setAttribute('name', 'remove');
-// cardNumber.setAttribute('value', 'remove row');
-// row.appendChild(button);
-
 for (var i = 0; i <= rCount; i++) {
-
     var row = document.querySelector("table").insertRow(-1);
 
+    var cell1 = row.insertCell(-1);
+    var button = document.createElement("input");
+    button.setAttribute("type", "button")
+    button.setAttribute("name", "Delete");
+    button.setAttribute("value", "del");
+    button.setAttribute("onclick", `removeRow()`);
+    cell1.appendChild(button);
 
     for (var j = 0; j <= cCount; j++) {
         var letter = String.fromCharCode("A".charCodeAt(0) + j - 1);
